8af3bd6e54e42d527319e8de33f8516b7003eaf4
zezeg2 <whdgus003@gmail.com>
2022-12-23 15:06:22 +0900

    1222-수정

--- src/ch16/network/socket/assign/ClientRequest.java
+++ src/ch16/network/socket/assign/ClientRequest.java
@@ -2,6 +2,7 @@ package ch16.network.socket.assign;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -9,33 +10,44 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ClientRequest {
+    private static ClientRequest instance;
     private final Scanner sc = new Scanner(System.in);
-    private final DataInputStream in;
-    private final DataOutputStream out;
 
-    public ClientRequest(Socket socket) throws IOException {
-        in = new DataInputStream(socket.getInputStream());
-        out = new DataOutputStream(socket.getOutputStream());
+    public static ClientRequest getInstance() {
+        if (instance == null) {
+            instance = new ClientRequest();
+        }
+        return instance;
     }
 
-    public void post() throws IOException {
-        List<Product> products = new ArrayList<>();
-        while (true) {
-            products.add(new Product());
-            System.out.println("enter the 'y' to add another product");
-            if (!sc.next().equals("y")) break;
+    public ClientRequest() {
+    }
+
+    public void post(Socket socket) throws IOException {
+        try (DataInputStream in = new DataInputStream(socket.getInputStream());
+             DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+            List<Product> products = new ArrayList<>();
+            while (true) {
+                products.add(new Product());
+                System.out.println("enter the 'y' to add another product");
+                if (!sc.next().equals("y")) break;
+            }
+            out.writeUTF(products.stream()
+                    .map(product -> String.format("%8s%8s%8s", product.name, product.price, product.stock))
+                    .reduce("", (s1, s2) -> s1 + s2 + "\n"));
+            sc.close();
+            System.out.println(in.readUTF());
+        }
+    }
+
+    public void get(Socket socket) throws IOException {
+        try (DataInputStream in = new DataInputStream(socket.getInputStream());
+             DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+            System.out.println("Enter the Product name");
+            out.writeUTF(sc.next());
+            System.out.println("Result : " + in.readUTF());
+        } catch (EOFException e) {
         }
 
-        out.writeUTF(products.stream()
-                .map(product -> String.format("%8s%8s%8s", product.name, product.price, product.stock))
-                .reduce("", (s1, s2) -> s1 + s2 + "\n"));
-        sc.close();
-        System.out.println(in.readUTF());
-    }
-
-    public void get() throws IOException {
-        System.out.println("조회하실 상품명을 입력해주세요");
-        out.writeUTF(sc.next());
-        System.out.println("Result : " + in.readUTF());
     }
 }
--- src/ch16/network/socket/assign/ProductTCPClient.java
+++ src/ch16/network/socket/assign/ProductTCPClient.java
@@ -6,24 +6,23 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class ProductTCPClient {
-    static Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) throws IOException {
-        try(Socket socket = new Socket("localhost", 19999);
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream())){
-            ClientRequest request = new ClientRequest(socket);
+        try (Scanner sc = new Scanner(System.in);
+             Socket socket = new Socket("localhost", 19999);
+             DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+            ClientRequest request = ClientRequest.getInstance();
             System.out.println("Enroll Product : '1', Search Product : '2', Quit : 'q'");
             while (true) {
                 String key = sc.next();
                 if (key.equals("q")) break;
                 if (key.equals("1")) {
                     out.writeUTF(key);
-                    request.post();
+                    request.post(socket);
                     break;
                 }
                 if (key.equals("2")) {
                     out.writeUTF(key);
-                    request.get();
+                    request.get(socket);
                     break;
                 }
                 System.out.println("Enter correct command...\n");
