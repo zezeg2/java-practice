a817615c870806713aa4b3c922879149e3b7c3bd
jonghyeon <whdgus003@gmail.com>
2022-12-31 16:48:10 +0900

    refactoring jdbc crud assignment

--- jdbc-practice/src/main/java/template/MemberRunner.java
+++ jdbc-practice/src/main/java/template/MemberRunner.java
@@ -10,10 +10,14 @@ public class MemberRunner {
     public static void main(String[] args) {
         try (Scanner sc = GlobalScanner.getInstance().getScanner()) {
             System.out.println("===== 회원 관리 프로그램 =====");
+            System.out.print("\n1. 회원정보 입력\n2. 회원정보 수정\n3. 회원 탈퇴\n4. 회원 정보 조회\n5. 전체 회원 조회\n6. 메뉴보기\n0. 프로그램 종료\n번호 입력 > ");
             while (true) {
-                System.out.print("\n1. 회원정보 입력\n2. 회원정보 수정\n3. 회원 탈퇴\n4. 회원 정보 조회\n5. 전체 회원 조회\n6. 프로그램 종료\n번호 입력 > ");
+                while (!sc.hasNextInt()) {
+                    sc.next();
+                    System.err.print("올바른 값을 입력해주세요. 재 선택 > ");
+                }
                 int select = sc.nextInt();
-                if (select == 6) {
+                if (select == 0) {
                     System.out.println("====== 프로그램 종료 ======");
                     break;
                 }
@@ -24,7 +28,8 @@ public class MemberRunner {
                         case 3 -> DeleteMemberViewImpl.getInstance().run();
                         case 4 -> GetMemberInfoViewImpl.getInstance().run();
                         case 5 -> GetAllMemberInfoViewImpl.getInstance().run();
-                        default -> System.out.println("다시 입력해 주세요");
+                        case 6 -> System.out.print("\n1. 회원정보 입력\n2. 회원정보 수정\n3. 회원 탈퇴\n4. 회원 정보 조회\n5. 전체 회원 조회\n6. 메뉴보기\n0. 프로그램 종료\n번호 입력 > ");
+                        default -> System.err.print("올바른 값을 입력해주세요. 재 선택 > ");
                     }
                 } catch (RuntimeException e) {
                     System.out.println(e.getMessage());
